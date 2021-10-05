f9617bc608ba471a226a799816f79bcc4489a54d
Chamika Deshan Jayasingha <56592121+ChamikaD97@users.noreply.github.com>
2021-10-05 15:07:33 +0530

    Update Assignment 2(Q1).scala

--- Assignment 2(Q1).scala
+++ Assignment 2(Q1).scala
@@ -2,7 +2,6 @@
 A typical employee works 40 (normal) and 20(OT) hours per week has to pay 10% tax.
  Develop a functional program that determines the take home salary of an employee from the number of working hours and OT hours given.
 }*/
-
 object Assignment2 extends App {
 
   def normalSalary(hours :Int) = hours*150
